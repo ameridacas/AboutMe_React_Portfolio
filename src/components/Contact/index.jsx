149fa5d4cb78da824ca2a41d09190a8f239b01ef
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-12-12 22:03:38 -0700

    Updating the components for contact// add work section to HomePage

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,1 +1,66 @@
+import './style.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log(formData);
+    // Reset the form after submission
+    setFormData({ name: '', email: '', message: '' });
+  };
+
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>Want to go back? <Link to="/">Go back to Home</Link></p>
+    </div>
+  );
+};
+
+export default Contact;
 
